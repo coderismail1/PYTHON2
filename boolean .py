e89081727bd0847b4dcf82d64d212c56cc1f5290
İSMAİL <83731487+coderismail1@users.noreply.github.com>
2021-06-06 00:39:43 +0300

    Add files via upload

---
+++ boolean .py
@@ -0,0 +1,36 @@
+"""
+bool1 = True
+bool2 = False
+
+print(type(bool1))
+print(type(bool2))
+
+a = 2
+b = True
+
+print(a > b)
+
+c = 0
+d = False
+
+print(c == d)
+"""
+"""
+age = 30
+bool3 = age >= 26
+
+print( bool3)
+
+"""
+"""
+a =['Ahmet','Ali','Hasan']
+b =['Ahmet','Ali','Hasan']
+print( a == b)
+print( a is b)
+"""
+"""
+print(18 > 12 and 22 > 20 ) # and kelimesi olan yerlerde her iki lkarşılaştırmanında aynı değeri vermesi gerekirrr
+
+"""
+
+print( not False )
