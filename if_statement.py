29804dc0f35caad71c50bee3816b390229c073cc
İSMAİL <83731487+coderismail1@users.noreply.github.com>
2021-06-11 00:17:56 +0300

    Add files via upload

---
+++ if_statement.py
@@ -0,0 +1,65 @@
+"""
+age = 20
+
+if age >= 18:
+    print('OY KULLANABİLİRSİNİZ')
+
+"""
+
+
+"""
+my_list = ['Ahmet','Ebru','İsmail']
+if 'Ebru' in my_list:
+    print('Listede Var')
+"""
+
+
+"""
+age = 20
+if age > 23:
+    print('OY KULLANABİLİRSİNİZ')
+else:
+    print('OY KULLANAMIYORSUNUZ')
+"""
+
+"""
+age = 30
+if age <= 2 :
+    print('%80 İndirimli')
+
+elif age <= 10:
+    print('%50 İndirimli')
+
+elif age <= 15:
+    print('%25 İndirimli')
+
+else:
+    print('İndirim Yok')
+
+"""
+
+"""
+age = int(input('Lütfen Yaşınızı Giriniz : '))
+if age <= 2 :
+    print('%80 İndirimli')
+
+elif age <= 10:
+    print('%50 İndirimli')
+
+elif age <= 15:
+    print('%25 İndirimli')
+
+
+else:
+    print('İndirim Yok')
+"""
+
+"""
+my_condition = 5
+
+if my_condition:
+    print('Koşul Doğru')
+else:
+    print('Koşul Yanlış')
+
+"""
